ac6948fbdd9a92d1c84812142c41d51822019df9
Ronald Record <github@ronrecord.com>
2022-01-07 10:50:20 -0800

    Do not delete previously built gdrive binaries in build-all script

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -13,8 +13,7 @@ else
 fi
 
 # Initialize bin dir
-rm -rf $BIN_PATH
-mkdir -p $BIN_PATH
+[ -d "${BIN_PATH}" ] || mkdir -p "${BIN_PATH}"
 
 # Build binary for each platform this system's version of GO supports
 for PLATFORM in `go tool dist list`
@@ -30,8 +29,8 @@ do
         unset GOARM
     fi
 
-    export GOOS=$GOOS
-    export GOARCH=$GOARCH
+    export GOOS=${GOOS}
+    export GOARCH=${GOARCH}
     GOPLAT="${GOOS}_${GOARCH}"
 
     # Skip some platforms
@@ -39,22 +38,22 @@ do
     [ "${GOPLAT}" == "ios_amd64" ] && continue
     [ "${GOPLAT}" == "ios_arm64" ] && continue
 
-    if [ -d ${BIN_PATH}/${GOPLAT} ]
+    if [ -d "${BIN_PATH}/${GOPLAT}" ]
     then
         echo "${BIN_PATH}/${GOPLAT} already built"
     else
         echo "Installing gdrive for ${GOPLAT} in ${GOPATH}"
         go install -ldflags '-w -s' github.com/prasmussen/gdrive@latest > /dev/null 2>&1
     fi
-    [ -d ${BIN_PATH}/${GOPLAT} ] || {
-        if [ -x ${BIN_PATH}/gdrive ]
+    [ -d "${BIN_PATH}/${GOPLAT}" ] || {
+        if [ -x "${BIN_PATH}/gdrive" ]
         then
-            [ -d ${BIN_PATH}/${GOPLAT} ] || mkdir -p ${BIN_PATH}/${GOPLAT}
-            if [ -x ${BIN_PATH}/${GOPLAT}/gdrive ]
+            [ -d "${BIN_PATH}/${GOPLAT}" ] || mkdir -p "${BIN_PATH}/${GOPLAT}"
+            if [ -x "${BIN_PATH}/${GOPLAT}/gdrive" ]
             then
                 echo "Duplicate build for ${GOPLAT} detected"
             else
-                mv ${BIN_PATH}/gdrive ${BIN_PATH}/${GOPLAT}/gdrive
+                mv "${BIN_PATH}/gdrive" "${BIN_PATH}/${GOPLAT}/gdrive"
             fi
         else
             echo "Could not locate output of build for ${GOPLAT}"
